3bc5f95ad6c6318b893f304b85dc6ee3cf9a4874
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-04-20 16:34:08 +0900

    second

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:share_quiz/presentation/application.dart';
+
+void main() {
+  runApp(Application());
+}
---
+++ lib/presentation/application.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:share_quiz/presentation/screen/home.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
