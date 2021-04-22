f18050310f652e65941498c3a8d0edb15f19c542
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-04-22 17:20:07 +0900

    適当にファイル追加

--- lib/presentation/application.dart
+++ lib/presentation/application.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:share_quiz/presentation/screen/home.dart';
 
 class Application extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
