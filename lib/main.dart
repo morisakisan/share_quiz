c8ba18557a669ed7b7605c15b26aae44b4798361
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-04-22 20:13:42 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:share_quiz/presentation/application.dart';
 
 void main() {
-  runApp(Application());
+  runApp(
+    ProviderScope(
+      child: Application(),
+    ),
+  );
 }
--- lib/presentation/application.dart
+++ lib/presentation/application.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:share_quiz/presentation/screen/home.dart';
 
-class Application extends StatelessWidget {
+class Application extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
