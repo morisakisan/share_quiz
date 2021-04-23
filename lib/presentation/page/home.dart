bfb48e4c4f808294fe267ea6ed7aab8f7dc95418
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-04-23 12:44:22 +0900

    commit

---
+++ lib/presentation/page/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class Home extends HookWidget {
+  final _tab = <Tab>[
+    Tab(text: '新着'),
+    Tab(text: 'トレンド'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: _tab.length,
+        child: Scaffold(
+            appBar: AppBar(
+                title: Text('クイズシェア'),
+                bottom: TabBar(
+                  tabs: _tab,
+                )),
+            drawer: _createDrawer(),
+            body: TabBarView(
+              children: [
+                Text('Car'),
+                Text('Car'),
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+              backgroundColor: Colors.green,
+            )));
+  }
+
+  Drawer _createDrawer() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Column(children: [
+              Text(
+                "シンゴ",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              CircleAvatar(
+                radius: 30.0,
+                // backgroundImage:
+                // NetworkImage(photoUrl),
+                backgroundColor: Colors.transparent,
+              )
+            ]),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('プロフィール'),
+            onTap: () {
+
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text('設定'),
+            onTap: () {
+
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text('プライバシーポリシー'),
+            onTap: () {
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ライセンス'),
+            onTap: () {
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ログアウト'),
+            onTap: () {
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
