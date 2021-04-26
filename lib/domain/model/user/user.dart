7b028f85f7ee70e24c86ffb094a3e31f98bfcacf
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-04-26 14:39:11 +0900

    user

---
+++ lib/domain/model/user/user.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'user.freezed.dart';
+
+@freezed
+abstract class User with _$User {
+  const factory User({
+    required int id,
+    required String name,
+    required String detail,
+    required String photoUrl,
+    required bool isLoading,
+  }) = _User;
+}
