dd40e9e4ed6b8bc2658c63b9f884adeb2b38feef
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-05-02 14:21:45 +0900

    no message

--- lib/presentation/page/quiz_detail.dart
+++ lib/presentation/page/quiz_detail.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:share_quiz/domain/quiz/quiz.dart';
 
 class QuizDetail extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments;
+    final quiz = ModalRoute.of(context)!.settings.arguments as Quiz;
     return Scaffold(
       appBar: AppBar(
         title: Text('クイズ詳細'),
       ),
-      body: Text(args.toString()),
+      body: Text(quiz.toString()),
     );
   }
 }
