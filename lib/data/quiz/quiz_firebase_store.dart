b538bb7157e09f77f0d0a525c52d92fa394ac751
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-05-24 12:48:06 +0900

    no message

--- lib/data/quiz/quiz_firebase_store.dart
+++ lib/data/quiz/quiz_firebase_store.dart
@@ -1,14 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class QuizFirebaseStore {
+  
   CollectionReference<Map<String, dynamic>> _getCollection() {
     return FirebaseFirestore.instance.collection('quiz');
   }
-
-  // Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> fetchNews() async {
-  //   var document = await _getCollection().get();
-  //   return document.docs;
-  // }
 
   Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>> fetchNews() {
     return _getCollection()
