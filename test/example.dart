bd3b19316f87393d9c659c71c24d59217611c538
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-05-26 17:27:51 +0900

    no message

---
+++ test/example.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'Counter increments smoke test',
+    (WidgetTester tester) async {
+      
+    },
+  );
+}
