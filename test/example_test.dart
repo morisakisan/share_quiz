95630ce3ce714105f079a52a850c7651907c5db8
森崎 真悟 <morisakisingo@yahoo.co.jp>
2021-06-04 15:06:18 +0900

    no message

--- test/example_test.dart
+++ test/example_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("example", () {
-    var a = 5 / 100;
-    assert(a == 0.05);
-  });
+  test(
+    "example",
+    () {
+      var a = 5 / 100;
+      assert(a == 0.05);
+    },
+  );
 }
