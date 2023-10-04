cc0585025849e49543355fd826b68da227dd0c52
森崎真悟 <>
2023-10-04 21:20:56 +0900

    no message

---
+++ lib/data/comment/comment_dto.dart
@@ -0,0 +1,27 @@
+// Flutter imports:
+import 'package:flutter/foundation.dart';
+
+// Package imports:
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../json_converter/timestamp_converter.dart';
+
+part 'comment_dto.freezed.dart';
+part 'comment_dto.g.dart';
+
+@freezed
+abstract class CommentDto with _$CommentDto {
+  const factory CommentDto({
+    @JsonKey(includeFromJson: false, includeToJson: false) String? id,
+    @JsonKey(name: 'quiz_id') required String quizId,
+    @JsonKey(name: 'content') required String content,
+    @JsonKey(name: 'user_id') required String userId,
+    @TimestampConverter()
+    @JsonKey(name: 'created_at')
+    required DateTime createdAt,
+  }) = _CommentDto;
+
+  factory CommentDto.fromJson(Map<String, dynamic> json) =>
+      _$CommentDtoFromJson(json);
+}
---
+++ lib/data/comment/comment_dto.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'comment_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CommentDtoImpl _$$CommentDtoImplFromJson(Map<String, dynamic> json) =>
+    _$CommentDtoImpl(
+      quizId: json['quiz_id'] as String,
+      content: json['content'] as String,
+      userId: json['user_id'] as String,
+      createdAt:
+          const TimestampConverter().fromJson(json['created_at'] as Timestamp),
+    );
+
+Map<String, dynamic> _$$CommentDtoImplToJson(_$CommentDtoImpl instance) =>
+    <String, dynamic>{
+      'quiz_id': instance.quizId,
+      'content': instance.content,
+      'user_id': instance.userId,
+      'created_at': const TimestampConverter().toJson(instance.createdAt),
+    };
