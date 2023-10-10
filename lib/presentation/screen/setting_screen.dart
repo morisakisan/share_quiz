4442c4cc55dfa00154edb8aa1fbd85db8fe9f380
森崎真悟 <>
2023-10-10 19:43:14 +0900

    no message

--- lib/presentation/screen/setting_screen.dart
+++ lib/presentation/screen/setting_screen.dart
@@ -2,13 +2,13 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_settings_ui/flutter_settings_ui.dart';
 import 'package:package_info/package_info.dart';
 
 // Project imports:
 import 'package:share_quiz/presentation/widget/widget_utils.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SettingScreen extends HookWidget {
   @override
