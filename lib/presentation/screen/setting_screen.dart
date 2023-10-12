48532ca0bc7e4959c8fcc61e471a656e95c99344
森崎真悟 <>
2023-10-12 12:01:19 +0900

    Revert "no message"
    This reverts commit 4bfa96f0cae532c5a0a7d3bce350dde97e7fce9b.

--- lib/presentation/screen/setting_screen.dart
+++ lib/presentation/screen/setting_screen.dart
@@ -13,7 +13,6 @@ import 'package:share_quiz/presentation/utility/widget_utils.dart';
 class SettingScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    aaa
     final appLocalizations = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: AppBar(
