eac9fc0fb330bfa93c7e9b15347b1023728a8167
森崎真悟 <>
2023-10-16 16:38:25 +0900

    no message

--- lib/data/repository_impl/setting_repository_impl.dart
+++ lib/data/repository_impl/setting_repository_impl.dart
@@ -1,11 +1,11 @@
 // Package imports:
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:rxdart/rxdart.dart';
 
 // Project imports:
 import 'package:share_quiz/data/user/user_firebase_store.dart';
 import 'package:share_quiz/domain/repository/setting_repository.dart';
 import '../../domain/models/setting/setting.dart';
-import 'package:rxdart/rxdart.dart';
 
 class SettingRepositoryImpl extends SettingRepository {
   final _userDataStore = UserFirebaseStore();
@@ -15,6 +15,7 @@ class SettingRepositoryImpl extends SettingRepository {
     return CombineLatestStream.combine2(
         Stream.fromFuture(PackageInfo.fromPlatform()),
         _userDataStore.listenToUserChanges(),
-            (info, user) => Setting(packageInfo: info, isLogin: user != null));
+            (info, user) => Setting(packageInfo: info, isLogin: user != null)
+    );
   }
 }
