05d38cfbb75d2f229e536c0859da578c8e0e34bf
森崎真悟 <>
2023-10-16 15:57:12 +0900

    modelを作成

---
+++ lib/domain/models/setting/setting.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+// Package imports:
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:share_quiz/domain/models/user/user_data.dart';
+
+// Project imports:
+
+part 'setting.freezed.dart';
+
+@freezed
+abstract class Setting with _$Setting {
+  const factory Setting({
+    required PackageInfo packageInfo,
+    required UserData userData,
+  }) = _Setting;
+}
