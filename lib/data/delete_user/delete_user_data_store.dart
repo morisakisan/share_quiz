c3f9a14eb05a8e7b8df5046f3501b63c325a1210
森崎真悟 <>
2023-10-23 14:05:23 +0900

    no message

---
+++ lib/data/delete_user/delete_user_data_store.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_functions/cloud_functions.dart';
+
+class DeleteUserDataStore {
+  Future<void> deleteUserAccount() {
+    final functions = FirebaseFunctions.instance;
+    return functions.httpsCallable('deleteUserAccount').call();
+  }
+}
