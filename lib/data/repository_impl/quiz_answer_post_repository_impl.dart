f87340ec0464b0e8658e97c646b41717c7dcc576
森崎真悟 <>
2023-10-24 20:52:33 +0900

    no message

--- lib/data/repository_impl/quiz_answer_post_repository_impl.dart
+++ lib/data/repository_impl/quiz_answer_post_repository_impl.dart
@@ -5,13 +5,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:share_quiz/data/firebase_auth/firebase_auth_store.dart';
 import '../../domain/repository/quiz_answer_post_repository.dart';
 import '../answer/answer_dto.dart';
-import '../answer/answer_firebase_store.dart';
 import '../quiz/quiz_dto.dart';
 import '../quiz/quiz_firebase_store.dart';
 
 class QuizAnswerPostRepositoryImpl extends QuizAnswerPostRepository {
-  final answerDatastore = AnswerFirebaseStore();
-  final firebaseAuthStore = FirebaseAuthStore();
+  final _firebaseAuthStore = FirebaseAuthStore();
 
   @override
   Future<void> post(String quizDocId, int select) {
@@ -27,7 +25,7 @@ class QuizAnswerPostRepositoryImpl extends QuizAnswerPostRepository {
           json["quiz_id"] = e.reference.id;
           return AnswerDto.fromJson(json);
         }).toList();
-        var user = firebaseAuthStore.getCurrentUser();
+        var user = _firebaseAuthStore.getCurrentUser();
         var isCorrect = updateQuizDto.correctAnswer == select;
         var answerJson = {
           "quiz_id": quizDocId,
