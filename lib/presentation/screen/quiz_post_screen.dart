575af2261692d7addd9bb5ada9ac10b57b73096f
森崎真悟 <>
2023-11-08 14:24:22 +0900

    no message

--- lib/presentation/screen/quiz_post_screen.dart
+++ lib/presentation/screen/quiz_post_screen.dart
@@ -17,9 +17,7 @@ import '../../provider/quiz_form_use_case_provider.dart';
 import '../../provider/quiz_post_use_case_provider.dart';
 
 class QuizPostScreen extends HookConsumerWidget {
-  final _formKey = GlobalKey<FormState>();
-
-  QuizPostScreen({super.key});
+  const QuizPostScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -27,14 +25,14 @@ class QuizPostScreen extends HookConsumerWidget {
     final List<Widget> children = [];
 
     final appLocalizations = AppLocalizations.of(context)!;
-    children.add(Scaffold(
-      appBar: AppBar(
-        title: Text(appLocalizations.enterQuizPrompt),
+    children.add(
+      Scaffold(
+        appBar: AppBar(
+          title: Text(appLocalizations.enterQuizPrompt),
+        ),
+        body: _QuizPostForm(),
       ),
-      body: SingleChildScrollView(
-        child: _form(context, ref),
-      ),
-    ));
+    );
 
     if (postState is AsyncLoading) {
       children.add(WidgetUtils.loadingScreen(context));
@@ -51,90 +49,97 @@ class QuizPostScreen extends HookConsumerWidget {
 
     return Stack(children: children);
   }
+}
 
-  Widget _form(BuildContext context, WidgetRef ref) {
+class _QuizPostForm extends HookConsumerWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     final quizFromUseCase = ref.watch(quizFormUseCaseProvider.notifier);
     final appLocalizations = AppLocalizations.of(context)!;
-    return Container(
-      margin: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              maxLength: 20,
-              decoration: InputDecoration(
-                labelText: appLocalizations.title,
-                hintText: appLocalizations.enterTitlePrompt,
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                maxLength: 20,
+                decoration: InputDecoration(
+                  labelText: appLocalizations.title,
+                  hintText: appLocalizations.enterTitlePrompt,
+                ),
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return appLocalizations.titleMissing;
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  quizFromUseCase.addTitle(value);
+                },
               ),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return appLocalizations.titleMissing;
-                }
-                return null;
-              },
-              onSaved: (value) {
-                quizFromUseCase.addTitle(value);
-              },
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              maxLength: 100,
-              decoration: InputDecoration(
-                labelText: appLocalizations.questionPrompt,
-                hintText: appLocalizations.enterQuestionPrompt,
+              const SizedBox(height: 16),
+              TextFormField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                maxLength: 100,
+                decoration: InputDecoration(
+                  labelText: appLocalizations.questionPrompt,
+                  hintText: appLocalizations.enterQuestionPrompt,
+                ),
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return appLocalizations.questionTextMissing;
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  quizFromUseCase.addQuestion(value);
+                },
               ),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return appLocalizations.questionTextMissing;
-                }
-                return null;
-              },
-              onSaved: (value) {
-                quizFromUseCase.addQuestion(value);
-              },
-            ),
-            const SizedBox(height: 16),
-            ImageFormField(
-              onSaved: (value) {
-                quizFromUseCase.addImage(value);
-              },
-            ),
-            const SizedBox(height: 16),
-            ChoicesFormField(
-              context,
-              onSaved: (value) {
-                quizFromUseCase.addChoices(value!.item1);
-                quizFromUseCase.addAnswer(value.item2);
-              },
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                if (!_formKey.currentState!.validate()) {
-                  return;
-                }
-                // 入力データが正常な場合の処理
-                _formKey.currentState!.save();
-                var postUseCase = ref.read(quizPostUseCaseProvider.notifier);
-                final quizFrom = ref.read(quizFormUseCaseProvider);
-                var post = QuizPostData(
-                  title: quizFrom.title!,
-                  question: quizFrom.question!,
-                  choices: quizFrom.choices!,
-                  answer: quizFrom.answer!,
-                  imageFile: quizFrom.image,
-                );
-                postUseCase.post(post);
-              },
-              child: Text(appLocalizations.postQuiz),
-            ),
-          ],
+              const SizedBox(height: 16),
+              ImageFormField(
+                onSaved: (value) {
+                  quizFromUseCase.addImage(value);
+                },
+              ),
+              const SizedBox(height: 16),
+              ChoicesFormField(
+                context,
+                onSaved: (value) {
+                  quizFromUseCase.addChoices(value!.item1);
+                  quizFromUseCase.addAnswer(value.item2);
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  if (!_formKey.currentState!.validate()) {
+                    return;
+                  }
+                  // 入力データが正常な場合の処理
+                  _formKey.currentState!.save();
+                  var postUseCase = ref.read(quizPostUseCaseProvider.notifier);
+                  final quizFrom = ref.read(quizFormUseCaseProvider);
+                  var post = QuizPostData(
+                    title: quizFrom.title!,
+                    question: quizFrom.question!,
+                    choices: quizFrom.choices!,
+                    answer: quizFrom.answer!,
+                    imageFile: quizFrom.image,
+                  );
+                  postUseCase.post(post);
+                },
+                child: Text(appLocalizations.postQuiz),
+              )
+            ],
+          ),
         ),
       ),
     );
