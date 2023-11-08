954db35e12dfce050c58ab3b026fe857fb081e6d
森崎真悟 <>
2023-11-08 17:12:49 +0900

    no message

--- lib/presentation/common/quiz_item.dart
+++ lib/presentation/common/quiz_item.dart
@@ -8,7 +8,6 @@ import '../nav.dart';
 import '../utility/widget_utils.dart';
 
 class QuizItem extends StatelessWidget {
-
   final Quiz quiz;
 
   const QuizItem(this.quiz, {super.key});
@@ -23,97 +22,68 @@ class QuizItem extends StatelessWidget {
       formatted = formatter.format(quiz.createdAt!);
     }
 
-    final List<Widget> list = [];
+    final correctRate = quiz.correctAnswerRate != null
+        ? appLocalizations.correctRateWithPercent(
+        (quiz.correctAnswerRate! * 100).toInt())
+        : "";
 
-    createImage(image) => Padding(
-      padding: const EdgeInsets.fromLTRB(8, 8, 16, 8),
-      child: image,
-    );
-
-    const imageSize = 125.0;
-    if (quiz.imageUrl != null) {
-      list.add(
-        createImage(WidgetUtils.getQuizImage(imageSize, quiz.imageUrl!)),
-      );
-    } else {
-      list.add(
-        createImage(WidgetUtils.getNoImage(context, imageSize)),
-      );
-    }
-
-    final String correctRate;
-    if (quiz.correctAnswerRate != null) {
-      correctRate = appLocalizations
-          .correctRateWithPercent((quiz.correctAnswerRate! * 100).toInt());
-    } else {
-      correctRate = "";
-    }
-
-    list.add(
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 8,
-          ),
-          Text(
-            quiz.title,
-            style: theme.textTheme.headlineSmall,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(
-            appLocalizations.questionText(quiz.question),
-            style: theme.textTheme.titleLarge,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(
-            appLocalizations.answerCountWithRate(
-                quiz.answerCount ?? 0, correctRate),
-            style: theme.textTheme.bodyMedium,
-          ),
-        ],
-      ),
-    );
-
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: list,
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.start,
-            children: [
-              TextButton.icon(
-                icon: const Icon(Icons.question_answer_rounded),
-                onPressed: () {
-                  Navigator.of(context).pushNamed(
-                    Nav.quizDetail,
-                    arguments: quiz.documentId,
-                  );
-                },
-                label: Text(appLocalizations.answerTheQuiz),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-            child: Text(
-              formatted ?? "",
-              style: theme.textTheme.bodySmall,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushNamed(
+          Nav.quizDetail,
+          arguments: quiz.documentId,
+        );
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                if (quiz.imageUrl != null)
+                  Image.network(
+                    quiz.imageUrl!,
+                    width: 125.0,
+                    height: 125.0,
+                    fit: BoxFit.cover,
+                  ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          quiz.title,
+                          style: theme.textTheme.headlineSmall,
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          appLocalizations.questionText(quiz.question),
+                          style: theme.textTheme.bodyText2,
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          appLocalizations.answerCountWithRate(
+                              quiz.answerCount ?? 0, correctRate),
+                          style: theme.textTheme.caption,
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          formatted ?? "",
+                          style: theme.textTheme.overline,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-
 }
+
+
