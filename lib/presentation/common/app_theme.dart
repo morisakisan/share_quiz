22920ceeda2415c3d708d1cea44c9abcae411d49
森崎真悟 <>
2023-11-09 21:23:18 +0900

    no message

--- lib/presentation/common/app_theme.dart
+++ lib/presentation/common/app_theme.dart
@@ -4,34 +4,28 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final lightTheme = ThemeData(
     brightness: Brightness.light,
-
     primaryColor: Colors.yellow[800],
-
     colorScheme: ColorScheme.light(
       primary: Colors.yellow[800]!,
       secondary: Colors.orange[700]!,
     ),
-
     appBarTheme: AppBarTheme(
       color: Colors.yellow[800],
       elevation: 0,
     ),
-
     buttonTheme: ButtonThemeData(
       buttonColor: Colors.orange[700],
       textTheme: ButtonTextTheme.primary,
     ),
-
     floatingActionButtonTheme: FloatingActionButtonThemeData(
       foregroundColor: Colors.white,
       backgroundColor: Colors.yellow[900],
     ),
-
     textTheme: const TextTheme(
-      displayLarge: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black),
+      displayLarge: TextStyle(
+          fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black),
       bodyLarge: TextStyle(fontSize: 14.0, color: Colors.black87),
     ),
-
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(8.0),
@@ -46,7 +40,6 @@ class AppTheme {
         borderSide: BorderSide(color: Colors.orange[700]!),
       ),
     ),
-
     cardTheme: CardTheme(
       color: Colors.white,
       shadowColor: Colors.grey[50],
@@ -55,7 +48,6 @@ class AppTheme {
         borderRadius: BorderRadius.circular(10),
       ),
     ),
-
     tabBarTheme: TabBarTheme(
       labelColor: Colors.white, // 選択中のタブのテキスト色
       unselectedLabelColor: Colors.grey[300], // 非選択のタブのテキスト色
@@ -66,7 +58,18 @@ class AppTheme {
         ),
       ),
     ),
-
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: Colors.white, // ドロワーの背景色
+      elevation: 16.0, // ドロワーの影の高さ
+      shape: RoundedRectangleBorder(
+        // ドロワーの形状
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+        ),
+      ),
+      // その他の設定...
+    ),
   );
 
   static final ThemeData darkTheme = ThemeData(
@@ -102,7 +105,8 @@ class AppTheme {
 
     // テキストのテーマ設定
     textTheme: const TextTheme(
-      displayLarge: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
+      displayLarge: TextStyle(
+          fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
       bodyLarge: TextStyle(fontSize: 14.0, color: Colors.white70),
     ),
 
@@ -143,5 +147,17 @@ class AppTheme {
       ),
     ),
 
+    drawerTheme: DrawerThemeData(
+      backgroundColor: Colors.grey[850], // ドロワーの背景色
+      elevation: 16.0, // ドロワーの影の高さ
+      shape: const RoundedRectangleBorder(
+        // ドロワーの形状
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+        ),
+      ),
+      // その他の設定...
+    ),
   );
 }
