acb9a4dbf8773b216de68ae4cb111f4cb6692235
森崎真悟 <>
2023-11-16 16:12:43 +0900

    no message

--- lib/data/mapper/user_data_mapper.dart
+++ lib/data/mapper/user_data_mapper.dart
@@ -1,6 +1,3 @@
-// Package imports:
-import 'package:firebase_auth/firebase_auth.dart';
-
 // Project imports:
 import '../../domain/models/user/user_data.dart';
 import '../firestore/user/user_dto.dart';
