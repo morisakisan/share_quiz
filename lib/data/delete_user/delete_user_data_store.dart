63e12f4df65d6f9e07d6429409f23cefaef7b481
森崎真悟 <>
2023-12-11 21:11:38 +0900

    no message

--- lib/data/delete_user/delete_user_data_store.dart
+++
@@ -1,9 +0,0 @@
-// Package imports:
-import 'package:cloud_functions/cloud_functions.dart';
-
-class DeleteUserDataStore {
-  Future<void> deleteUserAccount() {
-    final functions = FirebaseFunctions.instance;
-    return functions.httpsCallable('deleteUserAccount').call();
-  }
-}
--- lib/data/repository_impl/delete_user_repository_impl.dart
+++ lib/data/repository_impl/delete_user_repository_impl.dart
@@ -1,6 +1,5 @@
 // Project imports:
 import '../../domain/repository/delete_user_repository.dart';
-import '../delete_user/delete_user_data_store.dart';
 import '../firebase_auth/firebase_auth_store.dart';
 import '../google_sign_in/google_signin_store.dart';
 
@@ -8,7 +7,6 @@ class DeleteUserRepositoryImpl extends DeleteUserRepository {
 
   @override
   Future<void> delete() async {
-    await DeleteUserDataStore().deleteUserAccount();
     await FirebaseAuthStore().signOut();
     await GoogleSignInStore().signOut();
   }
