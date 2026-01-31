521a318f49695f33579150c7013931d92bf626a8
森崎真悟 <>
2026-01-31 16:44:20 +0900

    no message

--- test/example_test.dart
+++ test/example_test.dart
@@ -3,10 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test(
-    "test",
+    "example",
         () {
       var a = 5 / 100;
       assert(a == 0.05);
     },
   );
 }
+
