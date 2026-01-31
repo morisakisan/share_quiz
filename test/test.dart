8ccb8f12f7f7d1c335cbbadf83e6b9c432f24db5
森崎真悟 <>
2026-01-31 16:34:07 +0900

    no message

---
+++ test/test.dart
@@ -0,0 +1,12 @@
+// Package imports:
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    "test",
+        () {
+      var a = 5 / 100;
+      assert(a == 0.05);
+    },
+  );
+}
